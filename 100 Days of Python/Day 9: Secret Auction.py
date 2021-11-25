27411ad653f78a97072e07111943735e8705756b
Gnoot01 <86306864+Gnoot01@users.noreply.github.com>
2021-11-25 13:32:52 +0800

    import os, os.system("clear") to clear screen

---
+++ 100 Days of Python/Day 9: Secret Auction.py
@@ -0,0 +1,52 @@
+from SecretAuctionArt import logo
+print(logo)
+
+import os
+import random
+
+bids={}
+end=False
+while not end:
+    name=input("What is your name? ")
+    price=float((input("What is your bid?: $")))
+    bids[name]=price
+    while True:
+        condition=input("Are there any other bidders? y/n ").lower()
+        if condition=="n":
+            end=True
+            break
+        elif condition=="y":
+            break
+        else: print("Only y/n please! ")
+    os.system("clear")
+
+highestBid=0
+highestBidder=""
+bidContestants=[]
+for bidder in bids:
+    if bids[bidder]>highestBid:
+        highestBid=bids[bidder]
+        highestBidder=bidder
+        bidContestants=[]
+    elif bids[bidder]==highestBid:
+        highestBid=bids[bidder]
+        bidContestants.append(bidder)
+
+if len(bidContestants)>0: highestBidder=random.choice(bidContestants)
+print(f"The winner is {highestBidder} with a bid of ${'{:.2f}'.format(highestBid)}. Congrats!")
+
+######################################################################################################
+# SecretAuctionArt.py
+logo = '''
+                         ___________
+                         \         /
+                          )_______(
+                          |"""""""|_.-._,.---------.,_.-._
+                          |       | | |               | | ''-.
+                          |       |_| |_             _| |_..-'
+                          |_______| '-' `'---------'` '-'
+                          )"""""""(
+                         /_________\\
+                       .-------------.
+                      /_______________\\
+'''
