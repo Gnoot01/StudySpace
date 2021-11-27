61abc1ef7bca77ddf54bd010af0e86bb4dfa86b4
Gnoot01 <86306864+Gnoot01@users.noreply.github.com>
2021-11-27 17:37:15 +0800

    list.index(sthFoundInList) to get index

---
+++ 100 Days of Python/Day 8 Caesar Cipher/main.py
@@ -0,0 +1,26 @@
+from art import logo
+print(logo)
+
+def caesar(text, shift, instruction):
+    result = ""
+    if instruction=="decode": shift*=-1
+    for ch in text:
+        if ch not in alphabet: result+=ch
+        #Simpler: else: result+=alphabet[(alphabet.index(ch)+shift)%26]
+        for i in range(len(alphabet)):
+            if ch == alphabet[i]:result+=alphabet[(i+shift)%26]
+    print(f"The resulting text is {result}")
+
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+shouldEnd = False
+while not shouldEnd:
+    instruction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
+    assert instruction=="encode" or instruction=="decode"
+    text = input("Type your message:\n").lower()
+    shift = int(input("Type the shift number:\n"))
+    caesar(text, shift, instruction)
+    restart = input("Type 'yes' if you want to go again. Otherwise type 'no'.\n").lower()
+    assert restart=="yes" or restart=="no"
+    if restart == "no":
+        shouldEnd = True
+        print("Goodbye")
