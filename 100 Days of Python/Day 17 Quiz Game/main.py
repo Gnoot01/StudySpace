2d274eb91e28affb17c3d08e01736044bbb15ba5
Gnoot01 <86306864+Gnoot01@users.noreply.github.com>
2021-11-27 23:47:25 +0800

    OOP, Class & Objects, __init__
    Attributes aka Class variables, Methods aka Class functions

---
+++ 100 Days of Python/Day 17 Quiz Game/main.py
@@ -0,0 +1,16 @@
+from question_model import Question
+from data import question_data
+# This is quite nice, I thought I needed web scraping knowledge first to get data like this, but apparently I could just
+# get them from https://opentdb.com/api_config.php for a trivia DB! Nice! I can actually play this :)
+from quiz_brain import QuizBrain
+
+question_bank = []
+for question_set in question_data:
+    question_bank.append(Question(question_set["question"], question_set["correct_answer"]))
+
+quiz = QuizBrain(question_bank)
+
+while quiz.still_has_questions(): quiz.get_questions()
+quiz.end()
+
+
---
+++ 100 Days of Python/Day 17 Quiz Game/quiz_brain.py
@@ -0,0 +1,29 @@
+class QuizBrain:
+    def __init__(self, questions_list):
+        self.question_number = 0
+        self.score = 0
+        self.questions_list = questions_list
+
+    def check_answer(self, current_question_set, user_answer):
+        if user_answer == current_question_set.answer:
+            self.question_number += 1
+            self.score += 1
+            print("You got it right!")
+        else:
+            self.question_number += 1
+            print("You got it wrong!")
+        print(f"The correct answer was: {current_question_set.answer}.\nYour current score is: {self.score}/{self.question_number}.\n")
+
+    def get_questions(self):
+        current_question_set = self.questions_list[self.question_number]
+        user_answer = input(f"Q{self.question_number+1}: {current_question_set.text} ([T]rue/[F]alse: ").upper()
+        # ALWAYS need to use self. ... if referencing an attribute/method within the same class!
+        self.check_answer(current_question_set, user_answer)
+
+    def still_has_questions(self):
+        return self.question_number < len(self.questions_list)
+
+    def end(self):
+        print("You've completed the quiz")
+        print(f"Your final score was {self.score}/{self.question_number}")
+
