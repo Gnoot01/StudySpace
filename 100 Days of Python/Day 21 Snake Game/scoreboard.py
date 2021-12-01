c85aa89f01da5b57bb66355bfb7a163aea4d8343
Gnoot01 <86306864+Gnoot01@users.noreply.github.com>
2021-12-01 23:13:10 +0800

    Screen tracing, update, time.sleep, listen, slicing, Inheritance

--- 100 Days of Python/Day 21 Snake Game/scoreboard.py
+++ 100 Days of Python/Day 21 Snake Game/scoreboard.py
@@ -11,12 +11,23 @@ class ScoreBoard(Turtle):
         self.penup()
         self.goto(0, 360)
         self.score = 0
-        self.write(f"Score: {self.score}", align=ALIGNMENT, font=FONT)
+        with open("data.txt", 'r') as handle:
+            self.high_score = int(handle.read())
+        self.update_score()
+
+    def update_score(self):
+        self.clear()
+        self.write(f"Score: {self.score} High Score: {self.high_score}", align=ALIGNMENT, font=FONT)
 
     def increase_score(self):
-        self.clear()
         self.score += 1
-        self.write(f"Score: {self.score}", align=ALIGNMENT, font=FONT)
+        self.update_score()
+
+    def reset(self):
+        if self.score > self.high_score:
+            self.high_score = self.score
+            with open("data.txt", 'w') as handle: handle.write(str(self.high_score))
+        self.score = 0
 
     def game_over(self):
         self.goto(-20, 0)
