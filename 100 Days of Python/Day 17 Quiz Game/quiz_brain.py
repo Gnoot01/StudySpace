e6572927fc851717d0d1338595d526ac1466532c
Gnoot01 <86306864+Gnoot01@users.noreply.github.com>
2021-12-10 23:21:20 +0800

    import html+html.unescape() for html entities (',")

--- 100 Days of Python/Day 17 Quiz Game/quiz_brain.py
+++ 100 Days of Python/Day 17 Quiz Game/quiz_brain.py
@@ -1,3 +1,6 @@
+import html
+
+
 class QuizBrain:
     def __init__(self, questions_list):
         self.question_number = 0
@@ -16,7 +19,7 @@ class QuizBrain:
 
     def get_questions(self):
         current_question_set = self.questions_list[self.question_number]
-        user_answer = input(f"Q{self.question_number+1}: {current_question_set.text} ([T]rue/[F]alse: ").upper()
+        user_answer = input(f"Q{self.question_number+1}: {html.unescape(current_question_set.text)} ([T]rue/[F]alse: ").upper()
         # ALWAYS need to use self. ... if referencing an attribute/method within the same class!
         self.check_answer(current_question_set, user_answer)
 
