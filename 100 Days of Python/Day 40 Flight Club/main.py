3a6bf347eed5c10464f7cbb6fdae6dc06dbfcbde
Gnoot01 <86306864+Gnoot01@users.noreply.github.com>
2021-12-14 16:46:53 +0800

    'pass' in class __init__

--- 100 Days of Python/Day 40 Flight Club/main.py
+++ 100 Days of Python/Day 40 Flight Club/main.py
@@ -7,11 +7,11 @@ flight_search = FlightSearch()
 data_manager = DataManager()
 notification_manager = NotificationManager()
 emails = [row["email"] for row in data_manager.get_users_data()]
-"""Populating Google Sheet IATA codes wrt city names"""
-# iata_codes = [flight_search.get_IATA_code(row["city"]) for row in data_manager.get_prices_data()]
-# lowest_prices = [row["lowestPrice"] for row in data_manager.get_prices_data()]
-iata_codes = ['PAR', 'BER', 'TYO', 'SYD', 'IST', 'MFM', 'NYC', 'SFO', 'CPT', 'RGN', 'HKG', 'CNX', 'BUQ', 'PNH', 'HIJ', 'PER', 'AMS', 'DPS', 'SEL', 'CAI', 'ATH', 'SZX', 'TPE']
-lowest_prices = [700, 710, 550, 450, 850, 450, 700, 825, 800, 450, 246, 220, 1300, 200, 650, 450, 625, 420, 400, 620, 650, 1500, 300]
+"""Populating Google Sheet IATA codes wrt city names (Uses Sheetly requests quota!)"""
+iata_codes = [flight_search.get_IATA_code(row["city"]) for row in data_manager.get_prices_data()]
+lowest_prices = [row["lowestPrice"] for row in data_manager.get_prices_data()]
+# iata_codes = ['PAR', 'BER', 'TYO', 'SYD', 'IST', 'MFM', 'NYC', 'SFO', 'CPT', 'RGN', 'HKG', 'CNX', 'BUQ', 'PNH', 'HIJ', 'PER', 'AMS', 'DPS', 'SEL', 'CAI', 'ATH', 'SZX', 'TPE']
+# lowest_prices = [700, 710, 550, 450, 850, 450, 700, 825, 800, 450, 246, 220, 1300, 200, 650, 450, 625, 420, 400, 620, 650, 1500, 300]
 for i in range(len(iata_codes)): data_manager.edit_prices_data(i+2, iata_codes[i])
 
 """Checking cheapest flights from tomorrow to 6 months later for all the cities with price lower than lowest price listed in the Google Sheet
@@ -38,8 +38,7 @@ for deal in deals:
 # last_name = input("What is your last name?\n").title()
 # email = input("What is your email?\n")
 # confirm_email = input("Type your email again.\n")
-# if email == confirm_email:
-#     data_manager.save_users_data(first_name, last_name, email)
+# if email == confirm_email: data_manager.save_users_data(first_name, last_name, email)
 # else: print("Sorry, your emails did not match")
 
 
