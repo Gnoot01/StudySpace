8f88cffae376e73f45e1b7040b23984a02d449f5
Andrew <86306864+Gnoot01@users.noreply.github.com>
2023-09-05 00:27:04 +0800

    Add files via upload

---
+++ Flutter/exercise_6_fav_places_app/android/app/src/main/kotlin/com/example/exercise_6_fav_places_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.exercise_6_fav_places_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ Flutter/exercise_6_fav_places_app/lib/providers/user_places.dart
@@ -0,0 +1,77 @@
+import 'dart:io';
+
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:path_provider/path_provider.dart' as syspaths;
+import 'package:path/path.dart' as path;
+import 'package:sqflite/sqflite.dart' as sql;
+import 'package:sqflite/sqlite_api.dart';
+
+import 'package:exercise_6_fav_places_app/models/place.dart';
+
+Future<Database> _getDatabase() async {
+  // Returns path where db may be created
+  final dbPath = await sql.getDatabasesPath();
+  final db = await sql.openDatabase(
+    // Create db at specified path with specified name
+    path.join(dbPath, 'places.db'),
+    onCreate: (db, version) {
+      return db.execute(
+          'CREATE TABLE user_places(id TEXT PRIMARY KEY, title TEXT, image TEXT, lat REAL, lng REAL, address TEXT)');
+    },
+    // Change to create new version of db if db changes
+    version: 1,
+  );
+  return db;
+}
+
+class UserPlacesNotifier extends StateNotifier<List<Place>> {
+  UserPlacesNotifier() : super(const []);
+
+  Future<void> loadPlaces() async {
+    final db = await _getDatabase();
+    final data = await db.query('user_places');
+    final places = data
+        .map(
+          (row) => Place(
+            id: row['id'] as String,
+            title: row['title'] as String,
+            image: File(row['image'] as String),
+            location: PlaceLocation(
+              latitude: row['lat'] as double,
+              longitude: row['lng'] as double,
+              address: row['address'] as String,
+            ),
+          ),
+        )
+        .toList();
+
+    state = places;
+  }
+
+  void addPlace(String title, File image, PlaceLocation location) async {
+    // Returns app directory
+    final appDir = await syspaths.getApplicationDocumentsDirectory();
+    final filename = path.basename(image.path);
+    final copiedImage = await image.copy('${appDir.path}/$filename');
+
+    final newPlace =
+        Place(title: title, image: copiedImage, location: location);
+
+    final db = await _getDatabase();
+    db.insert('user_places', {
+      'id': newPlace.id,
+      'title': newPlace.title,
+      'image': newPlace.image.path,
+      'lat': newPlace.location.latitude,
+      'lng': newPlace.location.longitude,
+      'address': newPlace.location.address,
+    });
+
+    state = [newPlace, ...state];
+  }
+}
+
+final userPlacesProvider =
+    StateNotifierProvider<UserPlacesNotifier, List<Place>>(
+  (ref) => UserPlacesNotifier(),
+);
